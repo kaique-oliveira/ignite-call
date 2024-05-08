05b663b3a2f2d377ad70836e08dcb04ceb2433c4
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-05-08 15:44:05 -0300

    style: finalizado estilização da page register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
-import { Heading, MultiStep, Text } from "@ignite-ui/react";
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
 import { Container, Form, Header } from "./styles";
+import { ArrowRight } from "@phosphor-icons/react";
 
 export default function Register() {
   return (
@@ -14,7 +15,26 @@ export default function Register() {
         <MultiStep size={4} currentStep={1} />
       </Header>
 
-      <Form></Form>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-usuário"
+            crossOrigin=""
+          />
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" crossOrigin="" />
+        </label>
+
+        <Button type="submit">
+          {" "}
+          Próximo passo <ArrowRight />
+        </Button>
+      </Form>
     </Container>
   );
 }
